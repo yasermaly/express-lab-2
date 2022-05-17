860387903d0429062e529c7e36044f721f34e4a6
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-05-16 20:59:48 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/greeting/:name", (req, res) => {
   res.send("What's up " + req.params.name + "! It's great to see you!");
 })
 
+// Tip Calculator
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send("Tip = " + (req.params.total / req.params.tipPercentage));
+});
+
 app.listen(port, () => {
   console.log("Listening on port", port)
 });
