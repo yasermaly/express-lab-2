8676c4392e8431aec6967f904655effd16123e0e
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-05-16 20:49:19 -0700

    greeting express application created

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/:name", (req, res) => {
-  res.send()
+// Greeting
+app.get("/greeting/:name", (req, res) => {
+  res.send("What's up " + req.params.name + "! It's great to see you!");
 })
 
 app.listen(port, () => {
